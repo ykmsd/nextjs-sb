a32065c17dbabe25c02052a5e243a8e4f307dadd
ggdaltoso <ggdaltoso@gmail.com>
2023-03-03 08:39:13 -0300

    web-publisher: Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -10,23 +10,23 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!!</a>
+          Welcome to <a href="https://nextjs.org">my blog</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The home for all my 
+          <code className={styles.code}>ideas</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>About me &rarr;</h3>
+            <p>Find in-depth information about me and myself.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Thoughts &rarr;</h3>
+            <p>A little about my learnings and what I'm thinking</p>
           </a>
 
           <a
@@ -34,16 +34,16 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>I have some live applications that could be usefull for you</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Portfolio &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Interested on what have I been worked on and already built?
             </p>
           </a>
         </div>
@@ -51,7 +51,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
+          Created with&nbsp;<b>WebPublisher</b>&nbsp;⚡️
         </a>
       </footer>
     </div>
