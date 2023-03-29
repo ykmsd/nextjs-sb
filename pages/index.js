37d139e707b7a6040b844555d57ace599ea254da
Gabriel Daltoso <ggdaltoso@gmail.com>
2023-03-29 15:43:18 -0300

    web-publisher: Update pages/index.js, styles/styles3.css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import styles3 from '../styles/styles3.css';
+import styles4 from '../styles/styles4.css';
+import styles5 from '../styles/styles5.css';
+import styles2 from '../styles/styles2.css';
 
 export default function Home() {
   return (
@@ -10,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">my blog!</a>
+          Welcome to <a href="https://nextjs.org">my blog</a>
         </h1>
 
         <p className={styles.description}>
