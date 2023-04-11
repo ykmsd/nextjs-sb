314608f9a13a33e005bee083af44bbe01342d697
Yuka Masuda <yuka1110m@gmail.com>
2023-04-11 10:54:19 +0200

    web-publisher: Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import styles2 from '../styles/Test.module.scss';
-import styles3 from '../styles/Test2.module.css';
-import styles4 from '../styles/Test3.module.css';
-import styles5 from '../styles/Test4.module.css';
-import styles6 from '../styles/Test5.module.css';
-
 
 export default function Home() {
   return (
@@ -16,7 +10,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">my blog</a>
+          Welcome to a<a href="https://nextjs.org">my blog</a>
         </h1>
 
         <p className={styles.description}>
